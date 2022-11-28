3a9de87ccf5745c47f5f10ac381cea4a4a2749ab
Nir Shopen <nirshopen@h-MacBook-Pro-sl-Nir.local>
2022-11-28 17:40:24 +0200

    adding server.c and client.c

---
+++ client.c
@@ -0,0 +1,63 @@
+// cc client.c -o client
+#include <stdio.h>
+#include <stdlib.h>
+#include <mach/mach.h>
+#include <bootstrap.h>
+#include <string.h>
+#include <unistd.h>
+
+typedef struct {
+	mach_msg_header_t header;
+	char body_str[32];
+} message;
+
+int main(void) {
+	int kr;
+	mach_port_name_t task = mach_task_self();
+
+	// Create bootstrap port
+	mach_port_t bootstrap_port;
+	kr = task_get_special_port(task, TASK_BOOTSTRAP_PORT, &bootstrap_port);
+
+	if (kr != KERN_SUCCESS) {
+		return -1;
+	}
+
+	printf("[*] Got special bootstrap port: 0x%x\n", bootstrap_port);
+
+	// Get port to send to the com.echo.macherino.as-a-service and store it in port
+	mach_port_t port;
+	kr = bootstrap_look_up(bootstrap_port, "com.echo.macherino.as-a-service", &port);
+
+	if (kr != KERN_SUCCESS) {
+		return -2;
+	}
+
+	printf("[*] Port for com.echo.macherino.as-a-service: 0x%x\n", port);
+
+	// Create the message for sending
+	message msg = {0};
+	msg.header.msgh_remote_port = port;
+
+    // MACH_MSG_TYPE_COPY_SEND: The message will carry a send right, and the caller should supply a send right. 
+	msg.header.msgh_bits = MACH_MSGH_BITS_SET(
+		MACH_MSG_TYPE_COPY_SEND,
+		0,
+		0,
+		0);
+	msg.header.msgh_id = 4;
+	msg.header.msgh_size = sizeof(msg);
+
+	strcpy(msg.body_str, "test message");
+
+	mach_msg_return_t ret = mach_msg(
+		(mach_msg_header_t *)&msg,
+		MACH_SEND_MSG,
+		sizeof(msg),
+		0,
+		MACH_PORT_NULL,
+		MACH_MSG_TIMEOUT_NONE,
+		MACH_PORT_NULL);
+		
+	return ret;
+}
---
+++ server.c
@@ -0,0 +1,84 @@
+// cc server.c -o server
+#include <bootstrap.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mach/mach.h>
+
+typedef struct {
+	mach_msg_header_t header;
+	char body_str[32];
+	mach_msg_trailer_t trailer;
+} message;
+
+mach_msg_return_t receive_msg(mach_port_name_t);
+
+int main(void) {
+	int kr;
+    // The mach_task_self system call returns the calling thread's task port.
+    // mach_task_self has an effect equivalent to receiving a send right for the task port.
+	mach_port_t task = mach_task_self();
+    
+    // The mach_port_allocate function creates a new right in the specified task. 
+    // The mach_port_allocate parameters are task, right and name
+    // task parameter is the task acquiring the port right
+    // right is the kind of entity to be created. name is the task's name for the port right.
+	mach_port_name_t recv_port;
+	kr = mach_port_allocate(task, MACH_PORT_RIGHT_RECEIVE, &recv_port);
+
+	if (kr != KERN_SUCCESS)
+		return EXIT_FAILURE;
+
+	printf("[*] Created port with MACH_PORT_RIGHT_RECEIVE: 0x%x\n", recv_port);
+
+    // The mach_port_insert_right function inserts into task the caller's right for a port, using a specified name for the right in the target task
+	kr = mach_port_insert_right(task, recv_port, recv_port, MACH_MSG_TYPE_MAKE_SEND);
+
+	if (kr != KERN_SUCCESS)
+		return EXIT_FAILURE;
+
+	printf("[*] Added send right to the port\n");
+
+	mach_port_t bootstrap_port;
+	kr = task_get_special_port(task, TASK_BOOTSTRAP_PORT, &bootstrap_port);
+
+	printf("[*] Got bootstrap port: 0x%x\n", bootstrap_port);
+
+	if (kr != KERN_SUCCESS)
+		return EXIT_FAILURE;
+
+	kr = bootstrap_check_in(bootstrap_port, "com.echo.macherino.as-a-service", &recv_port);
+
+	if (kr != KERN_SUCCESS)
+		return EXIT_FAILURE;
+
+	printf("[*] Registered our service\n");
+		
+
+	while(true) {
+		mach_msg_return_t ret = receive_msg(recv_port);
+	}
+}
+
+mach_msg_return_t receive_msg(mach_port_name_t recv_port) {
+	message msg = {0};
+
+	mach_msg_return_t ret = mach_msg(
+		(mach_msg_header_t *)&msg,
+		MACH_RCV_MSG,
+		0,
+		sizeof(msg),
+		recv_port,
+		MACH_MSG_TIMEOUT_NONE,
+		MACH_PORT_NULL);
+
+	if (ret != MACH_MSG_SUCCESS)
+		return ret;
+
+	printf("got message\n");
+	printf("\tid: %d\n", msg.header.msgh_id);
+	printf("\tbodys: %s\n", msg.body_str);
+
+	return MACH_MSG_SUCCESS;
+}
+	
